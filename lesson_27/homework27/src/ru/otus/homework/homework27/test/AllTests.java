f8c70a59311f6d5d0ee67c8427f8ad4ec659fa58
wAngel825 <w.angel825@gmail.com>
2023-05-15 20:02:16 +0400

    lesson_27

--- lesson_27/homework27/src/ru/otus/homework/homework27/test/AllTests.java
+++ lesson_27/homework27/src/ru/otus/homework/homework27/test/AllTests.java
@@ -1,4 +1,22 @@
 package ru.otus.homework.homework27.test;
 
+import ru.otus.homework.homework27.test.unit.*;
+
 public class AllTests {
+    public static void main(String[] args) {
+        System.out.println("\n\n----------------------\n\n");
+
+        new Test01().testN01();
+        System.out.println("\n\n----------------------\n\n");
+
+        new Test02().testN02();
+        System.out.println("\n\n----------------------\n\n");
+
+        new Test03().testN03();
+        System.out.println("\n\n----------------------\n\n");
+
+        new Test04().testN04();
+        System.out.println("\n\n----------------------\n\n");
+
+    }
 }
